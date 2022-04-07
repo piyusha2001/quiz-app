adebd68ca63b3eaa78e7ab54d7d2f3ecf01c9534
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-08 01:15:38 +0530

    created basic layout and functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Card from './Components/Card';
 
 function App() {
-	return <div className='App'>Quiz</div>;
+	const startTrivia = async () => {};
+
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+	const nextQuestion = () => {};
+
+	return (
+		<div className='App'>
+			<h1>Fun CS Quiz</h1>
+			<button className='start' onClick={startTrivia}>
+				Start
+			</button>
+			<p className='score'>Score: </p>
+			<p>Loading Questions...</p>
+			<Card />
+			<button className='next' onClick={nextQuestion}>
+				Next
+			</button>
+		</div>
+	);
 }
 
 export default App;
