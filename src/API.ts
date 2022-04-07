5a98fdcf645847ebb47466ce82c6eb1637b44bff
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-08 01:50:22 +0530

    added category to api

--- src/API.ts
+++ src/API.ts
@@ -8,7 +8,7 @@ export const fetchQuizQuestions = async (
 	amount: number,
 	difficulty: Difficulty,
 ) => {
-	const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+	const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=18&difficulty=${difficulty}&type=multiple`;
 
 	const data = await (await fetch(endpoint)).json();
 	console.log(data);
