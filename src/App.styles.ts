c127358c2076234f1194a97074508b772b951151
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-08 20:48:06 +0530

    global style created

---
+++ src/App.styles.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+import BGImage from './images/compbg2.jpg';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+  body {
+    background-image: url(${BGImage});
+    background-size: cover;
+    margin: 0;
+    padding: 0 20px;
+    display: flex;
+    justify-content: center;
+  }
+  * {
+    box-sizing: border-box;
+
+  }
+`;
