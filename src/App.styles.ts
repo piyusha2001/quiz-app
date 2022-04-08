4ea020526156b6bd3f481b9f5c0d22cc7f7ffb84
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-08 21:15:18 +0530

    added wrapper for styling separate elements

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import BGImage from './images/compbg2.jpg';
 
 export const GlobalStyle = createGlobalStyle`
@@ -6,7 +6,8 @@ export const GlobalStyle = createGlobalStyle`
     height: 100%;
   }
   body {
-    background-image: url(${BGImage});
+   background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+		url(${BGImage});
     background-size: cover;
     margin: 0;
     padding: 0 20px;
@@ -17,4 +18,47 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
 
   }
+`;
+
+export const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	> p {
+		color: #00092c;
+	}
+	.score {
+		color: #fff;
+		font-size: 2rem;
+		margin: 0;
+	}
+	h1 {
+		font-family: 'Roboto Mono', monospace;
+		color: #fff;
+		background-size: 100%;
+		background-clip: text;
+		-webkit-background-clip: text;
+		font-size: 50px;
+		font-weight: 400;
+		text-align: center;
+		margin-top: 120px;
+	}
+	.start,
+	.next {
+		cursor: pointer;
+		background: linear-gradient(180deg, #fff, #ffcc91);
+		border: 2px solid #d38558;
+		box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+		border-radius: 10px;
+		height: 40px;
+		margin: 20px 0;
+		padding: 0 40px;
+		font-size: 1.5rem;
+		font-weight: 600;
+		width: 40%;
+	}
+	.start {
+		max-width: 400px;
+	}
 `;
