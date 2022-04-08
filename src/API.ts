4935987b8a0ca068b91919d0c05b316f9cabea45
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-08 15:56:15 +0530

    mapped data adding answers property

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,5 @@
+import { shuffleArray } from './utils';
+
 export type Question = {
 	category: string;
 	correct_answer: string;
@@ -22,5 +24,11 @@ export const fetchQuizQuestions = async (
 	const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=18&difficulty=${difficulty}&type=multiple`;
 
 	const data = await (await fetch(endpoint)).json();
-	console.log(data);
+	return data.results.map((question: Question) => ({
+		...question,
+		answers: shuffleArray([
+			...question.incorrect_answers,
+			question.correct_answer,
+		]),
+	}));
 };
