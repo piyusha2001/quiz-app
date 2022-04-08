21e404694fe5103bab7f41eb4c54996bee9f8312
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-08 15:30:13 +0530

    created layout for question card including props

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
-export default function Card() {
-	return <div>Card</div>;
-}
+type Props = {
+	question: string;
+	answers: string[];
+	callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+	userAnswer: any;
+	questionNr: number;
+	totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+	question,
+	answers,
+	callback,
+	userAnswer,
+	questionNr,
+	totalQuestions,
+}) => (
+	<div>
+		<p className='number'>
+			Question: {questionNr} / {totalQuestions}
+		</p>
+		<p dangerouslySetInnerHTML={{ __html: question }} />
+		<div>
+			{answers.map((answer) => (
+				<div key={answer}>
+					<button
+						disabled={userAnswer ? true : false}
+						value={answer}
+						onClick={callback}
+					>
+						<span dangerouslySetInnerHTML={{ __html: answer }} />
+					</button>
+				</div>
+			))}
+		</div>
+	</div>
+);
+
+export default QuestionCard;
